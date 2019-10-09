dc057ce0286fb436638ac5c3106ef3d2022dcca7
frlan <frlan@frlan.xpertiz.lu>
2019-10-09 14:30:34 +0200

    corrected a bug in init files

--- FDI_project/models/__init__.py
+++ FDI_project/models/__init__.py
@@ -1,14 +1,2 @@
 # -*- coding: utf-8 -*-
 
-from . import church_council
-from . import church_management_board
-from . import church_role
-from . import board_director
-from . import cac
-from . import res_partner
-from . import res_users
-from . import role_board
-from . import role_council
-from . import role_board_director
-from . import role_cac
-from . import file_exchange_log
