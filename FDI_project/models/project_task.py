331a791ecde8934cf710043a250ab2fd43fd3e23
frlan <frlan@frlan.xpertiz.lu>
2019-10-10 11:00:57 +0200

    test on filter

--- FDI_project/models/project_task.py
+++ FDI_project/models/project_task.py
@@ -5,5 +5,7 @@ from odoo import fields, models, api, _
 class CrossoveredBudget(models.Model):
     _inherit = 'project.task'
 
-    parent_level_1_id = fields.Many2one('project.task','Parent Niveau 1')
-    parent_level_2_id = fields.Boolean('project.task','Parent Niveau 2')
+    parent_level_1_id = fields.Many2one('project.task',string='Parent Niveau 1',
+                                        domain="[('project_id', '=', project_id)]")
+    parent_level_2_id = fields.Boolean('project.task',string='Parent Niveau 2',
+                                       domain="[('project_id', '=', project_id)]")
